90fe3a397aa83cd58a9785c63311f43837e85d56
newlukyanchikovv <lkostya2002@gmail.com>
2021-04-07 18:13:09 +0300

    MyFirstCommit

---
+++ cmake-build-debug/CMakeFiles/myTestProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myTestProject.dir/main.cpp.o"
+  "myTestProject"
+  "myTestProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myTestProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
