c748fa4615a03d1895db30c8762b0d56dfbb4c20
newlukyanchikovv <lkostya2002@gmail.com>
2021-04-08 16:13:08 +0300

    Third change in repository

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 
 using namespace std;
 
-int main() {
+int main() 
+{
+    int *a = new int[100];
+    for(int i = 0; i < 10; i++)
+    {
+        cin >> a[i];
+    }
     cout << "Hello, World!"<<endl;
     cout << "wow";
 }
